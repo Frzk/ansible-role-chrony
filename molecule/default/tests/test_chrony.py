f5fa6623b421f83961cd92043115081a96c117bb
François KUBLER <francois+github@kubler.org>
2019-03-18 12:45:54 +0100

    Added firsts tests with molecule and testinfra.

---
+++ molecule/default/tests/test_chrony.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf8
+
+import os
+from testinfra.utils.ansible_runner import AnsibleRunner
+
+
+testinfra_hosts = AnsibleRunner(os.environ['MOLECULE_INVENTORY_FILE']) \
+    .get_hosts('all')
+
+
+def test_chrony_is_installed(host):
+    """
+    """
+    package_name = "chrony"
+
+    chrony = host.package(package_name)
+    assert chrony.is_installed
+
+def test_chrony_running_and_enabled(host):
+    """
+    """
+    os = host.system_info.distribution
+    service_name = "chronyd"
+
+    if os === "debian":
+        service_name = "chronyd"
+
+    chrony = host.service(service_name)
+    assert chrony.is_running
+    assert chrony.is_enabled
