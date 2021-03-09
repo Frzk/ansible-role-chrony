4d459b2b82b077418933b194d91968e7317d2e31
François KUBLER <francois+github@kubler.org>
2021-03-09 10:02:54 +0100

    Fixed linting issues reported by our new linter workflow.
    And reported by `black` and `isort`.

--- molecule/default/tests/test_chrony.py
+++ molecule/default/tests/test_chrony.py
@@ -2,11 +2,10 @@
 # coding: utf8
 
 import os
+
 from testinfra.utils.ansible_runner import AnsibleRunner
 
-
-testinfra_hosts = AnsibleRunner(os.environ["MOLECULE_INVENTORY_FILE"])\
-                  .get_hosts("all")
+testinfra_hosts = AnsibleRunner(os.environ["MOLECULE_INVENTORY_FILE"]).get_hosts("all")
 
 
 def test_chrony_is_installed(host):
